da5d8bc7b1f73321070d770c4a8d874d12c599d1
Douaa yazidi <douaayazidi@gmail.com>
2020-08-02 20:08:24 +0100

    add components Parent and Son

---
+++ src/app/son/son.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-son',
+  templateUrl: './son.component.html',
+  styleUrls: ['./son.component.css']
+})
+export class SonComponent implements OnInit {
+  @Input() parentColor;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
