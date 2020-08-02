7261180d0db928dfa45130f84f58b5bef9e21df2
Douaa yazidi <douaayazidi@gmail.com>
2020-08-02 20:07:26 +0100

    add components Parent and Son

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { SonComponent } from './son/son.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    SonComponent
   ],
   imports: [
     BrowserModule,
